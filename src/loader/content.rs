da2464e9bd4efd9584d8bc6dceb8b5e2f94924b6
Maciej Jur <maciej@kamoshi.org>
2025-10-30 22:32:17 +0100

    fix glob_content

--- src/loader/content.rs
+++ src/loader/content.rs
@@ -1,11 +1,11 @@
 use crate::{
-    loader::{glob::GlobLoaderTask, File},
-    task::Handle,
     SiteConfig,
+    loader::{File, glob::GlobLoaderTask},
+    task::Handle,
 };
 use camino::Utf8PathBuf;
-use gray_matter::{engine::YAML, Matter};
-use serde::{de::DeserializeOwned, Deserialize};
+use gray_matter::{Matter, engine::YAML};
+use serde::{Deserialize, de::DeserializeOwned};
 use std::{fs, marker::PhantomData};
 
 #[derive(Deserialize)]
@@ -16,28 +16,29 @@ struct ContentData<T> {
     content: String,
 }
 
-pub struct Content<T> {
+#[derive(Clone)]
+pub struct Content<T: Clone> {
     pub path: Utf8PathBuf,
     pub metadata: T,
     pub content: String,
 }
 
-pub fn glob_content<T, G>(
+pub fn glob_content<T: Clone, G>(
     site_config: &mut SiteConfig<G>,
     path_base: &'static str,
     path_glob: &'static str,
-) -> Handle<Vec<Content<T>>>
+) -> Handle<Content<T>>
 where
     T: DeserializeOwned + Send + Sync + 'static,
     G: Send + Sync + 'static,
 {
-    let task = GlobLoaderTask::new(path_base, path_glob, move |_globals, file: File<Vec<u8>>| {
+    let task = GlobLoaderTask::new(path_base, path_glob, move |_, file: File<Vec<u8>>| {
+        let data = std::str::from_utf8(&file.metadata)?;
         let matter = Matter::<YAML>::new();
-        let parsed = matter.parse(std::str::from_utf8(&file.metadata)?);
-        let metadata: T = parsed.data.unwrap().deserialize()?;
+        let parsed = matter.parse::<T>(data)?;
         Ok(Content {
             path: file.path,
-            metadata,
+            metadata: parsed.data.unwrap(),
             content: parsed.content,
         })
     });
@@ -57,13 +58,10 @@ pub fn yaml<T: DeserializeOwned + Clone + Send + Sync + 'static, G: Send + Sync 
     site_config: &mut SiteConfig<G>,
     path: &'static str,
 ) -> Handle<T> {
-    site_config.add_task(
-        (),
-        move |_, _| -> T {
-            let content = fs::read_to_string(path).unwrap();
-            serde_yaml::from_str(&content).unwrap()
-        },
-    )
+    site_config.add_task((), move |_, _| -> T {
+        let content = fs::read_to_string(path).unwrap();
+        serde_yaml::from_str(&content).unwrap()
+    })
 }
 
 #[derive(Clone, Default)]
@@ -79,11 +77,8 @@ pub fn json<T: DeserializeOwned + Clone + Send + Sync + 'static, G: Send + Sync 
     site_config: &mut SiteConfig<G>,
     path: &'static str,
 ) -> Handle<T> {
-    site_config.add_task(
-        (),
-        move |_, _| -> T {
-            let content = fs::read_to_string(path).unwrap();
-            serde_json::from_str(&content).unwrap()
-        },
-    )
+    site_config.add_task((), move |_, _| -> T {
+        let content = fs::read_to_string(path).unwrap();
+        serde_json::from_str(&content).unwrap()
+    })
 }
